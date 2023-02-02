23a1f8a9f78f8ff4d1c48f1cee862b063f71d264
vrsri986 <80849633+vrsri986@users.noreply.github.com>
2023-02-01 19:31:10 -0800

    Sample aura component updates with telephony actions @W-12476050@ (#38)
    * Sample aura component updates with telephony actions
    
    * Sample aura component updates with telephony actions with endcall

--- ToolkitAPI/SampleAuraComponent/SampleComponentController.js
+++ ToolkitAPI/SampleAuraComponent/SampleComponentController.js
@@ -19,6 +19,14 @@
         helper.unmute(cmp);
     },
     
+    onMerge: function(cmp, event, helper) {
+        helper.merge(cmp);
+    },
+    
+    onSwap: function(cmp, event, helper) {
+        helper.swap(cmp);
+    },
+    
     onAcceptCall: function(cmp, event, helper) {
         helper.acceptCall(cmp);
     },
@@ -30,8 +38,56 @@
     onEndCall: function(cmp, event, helper) {
         helper.endCall(cmp);
     },
-
+    
     onStartPreviewCall: function(cmp, event, helper) {
         helper.startPreviewCall(cmp);
+    },
+    
+    onSendDigits: function(cmp, event, helper) {
+        helper.sendDigits(cmp);
+    },
+
+    onHold: function(cmp, event, helper) {
+        helper.hold(cmp);
+    },
+    
+    onResume: function(cmp, event, helper) {
+        helper.resume(cmp);
+    },
+    
+    onPauseRecording: function(cmp, event, helper) {
+        helper.pauseRecording(cmp);
+    },
+    
+    onResumeRecording: function(cmp, event, helper) {
+        helper.resumeRecording(cmp);
+    },
+    
+    onAddParticipant: function(cmp, event, helper) {
+        helper.addParticipant(cmp);
+    },
+    
+    onBlindTransfer: function(cmp, event, helper) {
+        helper.blindTransfer(cmp);
+    },
+    
+    handleComboboxHoldChange : function(cmp, event) {
+        cmp.set('v.comboBoxHoldValue', event.getParam("value"));
+    },
+    
+    handleComboboxResumeChange : function(cmp, event) {
+        cmp.set('v.comboBoxResumeValue', event.getParam("value"));
+    },
+    
+    handleComboboxRemoveParticipantChange : function(cmp, event) {
+        cmp.set('v.comboBoxRemoveParticipantValue', event.getParam("value"));
+    },
+    
+    handleComboboxAddParticipantChange : function(cmp, event) {
+        cmp.set('v.comboBoxAddParticipantValue', event.getParam("value"));
+    },
+    
+    handleComboboxBlindTransferChange : function(cmp, event) {
+        cmp.set('v.comboBoxBlindTransferValue', event.getParam("value"));
     }
 })
