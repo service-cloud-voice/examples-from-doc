a0c282730b90fd6d888c269562442d2ad60a1ae6
jake.elizondo <jake.elizondo@salesforce.com>
2024-05-29 10:10:19 -0700

    [W-12522289] fix: update external repo with changes from internal one

--- ToolkitAPI/SampleAuraComponent/SampleComponentController.js
+++ ToolkitAPI/SampleAuraComponent/SampleComponentController.js
@@ -1,93 +1,101 @@
 ({
-    onInit: function(cmp, event, helper) {
-        helper.subscribeToVoiceToolkit(cmp);
-    },
+  onInit: function (cmp, event, helper) {
+    helper.subscribeToVoiceToolkit(cmp);
+  },
 
-    onDestroy: function(cmp, event, helper) {
-        helper.unsubscribeFromVoiceToolkit(cmp);
-    },
-    
-    onInvokeNBA: function(cmp, event, helper) {
-        helper.updateNextBestActions(cmp);
-    },
-    
-    onMute: function(cmp, event, helper) {
-        helper.mute(cmp);
-    },
-      
-    onUnmute: function(cmp, event, helper) {
-        helper.unmute(cmp);
-    },
-    
-    onMerge: function(cmp, event, helper) {
-        helper.merge(cmp);
-    },
-    
-    onSwap: function(cmp, event, helper) {
-        helper.swap(cmp);
-    },
-    
-    onAcceptCall: function(cmp, event, helper) {
-        helper.acceptCall(cmp);
-    },
-    
-    onDeclineCall: function(cmp, event, helper) {
-        helper.declineCall(cmp);
-    },
-    
-    onEndCall: function(cmp, event, helper) {
-        helper.endCall(cmp);
-    },
-    
-    onStartPreviewCall: function(cmp, event, helper) {
-        helper.startPreviewCall(cmp);
-    },
-    
-    onSendDigits: function(cmp, event, helper) {
-        helper.sendDigits(cmp);
-    },
+  onDestroy: function (cmp, event, helper) {
+    helper.unsubscribeFromVoiceToolkit(cmp);
+  },
 
-    onHold: function(cmp, event, helper) {
-        helper.hold(cmp);
-    },
-    
-    onResume: function(cmp, event, helper) {
-        helper.resume(cmp);
-    },
-    
-    onPauseRecording: function(cmp, event, helper) {
-        helper.pauseRecording(cmp);
-    },
-    
-    onResumeRecording: function(cmp, event, helper) {
-        helper.resumeRecording(cmp);
-    },
-    
-    onAddParticipant: function(cmp, event, helper) {
-        helper.addParticipant(cmp);
-    },
-    
-    onBlindTransfer: function(cmp, event, helper) {
-        helper.blindTransfer(cmp);
-    },
-    
-    handleComboboxHoldChange : function(cmp, event) {
-        cmp.set('v.comboBoxHoldValue', event.getParam("value"));
-    },
-    
-    handleComboboxResumeChange : function(cmp, event) {
-        cmp.set('v.comboBoxResumeValue', event.getParam("value"));
-    },
-    
-    handleComboboxRemoveParticipantChange : function(cmp, event) {
-        cmp.set('v.comboBoxRemoveParticipantValue', event.getParam("value"));
-    },
-    
-    handleComboboxAddParticipantChange : function(cmp, event) {
-        cmp.set('v.comboBoxAddParticipantValue', event.getParam("value"));
-    },
-    
-    handleComboboxBlindTransferChange : function(cmp, event) {
-        cmp.set('v.comboBoxBlindTransferValue', event.getParam("value"));
-    }
-})
+  onSubscribe: function (cmp, event, helper) {
+    helper.subscribeToVoiceToolkit(cmp);
+  },
+
+  onUnsubscribe: function (cmp, event, helper) {
+    helper.unsubscribeFromVoiceToolkit(cmp);
+  },
+
+  onInvokeNBA: function (cmp, event, helper) {
+    helper.updateNextBestActions(cmp);
+  },
+
+  onMute: function (cmp, event, helper) {
+    helper.mute(cmp);
+  },
+
+  onUnmute: function (cmp, event, helper) {
+    helper.unmute(cmp);
+  },
+
+  onMerge: function (cmp, event, helper) {
+    helper.merge(cmp);
+  },
+
+  onSwap: function (cmp, event, helper) {
+    helper.swap(cmp);
+  },
+
+  onAcceptCall: function (cmp, event, helper) {
+    helper.acceptCall(cmp);
+  },
+
+  onDeclineCall: function (cmp, event, helper) {
+    helper.declineCall(cmp);
+  },
+
+  onEndCall: function (cmp, event, helper) {
+    helper.endCall(cmp);
+  },
+
+  onStartPreviewCall: function (cmp, event, helper) {
+    helper.startPreviewCall(cmp);
+  },
+
+  onSendDigits: function (cmp, event, helper) {
+    helper.sendDigits(cmp);
+  },
+
+  onHold: function (cmp, event, helper) {
+    helper.hold(cmp);
+  },
+
+  onResume: function (cmp, event, helper) {
+    helper.resume(cmp);
+  },
+
+  onPauseRecording: function (cmp, event, helper) {
+    helper.pauseRecording(cmp);
+  },
+
+  onResumeRecording: function (cmp, event, helper) {
+    helper.resumeRecording(cmp);
+  },
+
+  onAddParticipant: function (cmp, event, helper) {
+    helper.addParticipant(cmp);
+  },
+
+  onBlindTransfer: function (cmp, event, helper) {
+    helper.blindTransfer(cmp);
+  },
+
+  handleComboboxHoldChange: function (cmp, event) {
+    cmp.set("v.comboBoxHoldValue", event.getParam("value"));
+  },
+
+  handleComboboxResumeChange: function (cmp, event) {
+    cmp.set("v.comboBoxResumeValue", event.getParam("value"));
+  },
+
+  handleComboboxRemoveParticipantChange: function (cmp, event) {
+    cmp.set("v.comboBoxRemoveParticipantValue", event.getParam("value"));
+  },
+
+  handleComboboxAddParticipantChange: function (cmp, event) {
+    cmp.set("v.comboBoxAddParticipantValue", event.getParam("value"));
+  },
+
+  handleComboboxBlindTransferChange: function (cmp, event) {
+    cmp.set("v.comboBoxBlindTransferValue", event.getParam("value"));
+  },
+});
--- ToolkitAPI/sampleLWCComponent/sampleLWCComponent.js
+++ ToolkitAPI/sampleLWCComponent/sampleLWCComponent.js
@@ -1,219 +1,277 @@
-import { LightningElement, api, track } from 'lwc';
+import { getRecord } from "lightning/uiRecordApi";
+import { LightningElement, api, track, wire } from "lwc";
+
+const FIELDS = [
+  "VoiceCall.CallType",
+  "VoiceCall.FromPhoneNumber",
+  "VoiceCall.ToPhoneNumber",
+];
 
 export default class SampleLWCComponent extends LightningElement {
-    @track telephonyActionControlsDisabled = true;
+  @track isTelephonyActionControlsDisabled = true;
 
-    @api recordId;  
+  @api recordId;
 
-    payload = '{"key": "value"}';
-    teleEvent = 'No events received yet.';
-    transcript = 'No transcripts received yet.';
-    previewPhoneNumber = '';
-    addParticipantPhoneNumber = '';
-    sendDigits = '';
-    comboBoxHoldValue = 'Initial_Caller';
-    comboBoxResumeValue = 'Initial_Caller';
-    comboBoxRemoveParticipantValue = 'Initial_Caller';
-    comboBoxContactTypeValue = 'PhoneNumber';
-    comboBoxAddParticipantContactTypeValue = 'PhoneNumber';
-    hasRendered = false;
-    isBlindTransfer = false;
+  @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
+  voiceCall;
 
-    constructor() {
-        super();
-        this.telephonyEventListener = this.onTelephonyEvent.bind(this);
-        this.conversationEventListener = this.onConversationEvent.bind(this);
-    } 
+  get CallType() {
+    return this.voiceCall.data.fields.CallType.value;
+  }
 
-    renderedCallback() {
-        this.subscribeToVoiceToolkit();
+  get FromPhoneNumber() {
+    return this.voiceCall.data.fields.FromPhoneNumber.value;
+  }
+
+  get ToPhoneNumber() {
+    return this.voiceCall.data.fields.ToPhoneNumber.value;
+  }
+
+  payload = '{"key": "value"}';
+  teleEvent = "No events received yet.";
+  transcript = "No transcripts received yet.";
+  previewPhoneNumber = "";
+  addParticipantPhoneNumber = "";
+  sendDigits = "";
+  comboBoxHoldValue = "Initial_Caller";
+  comboBoxResumeValue = "Initial_Caller";
+  comboBoxRemoveParticipantValue = "Agent";
+  comboBoxContactTypeValue = "PhoneNumber";
+  comboBoxAddParticipantContactTypeValue = "PhoneNumber";
+  hasRendered = false;
+
+  constructor() {
+    super();
+    this.telephonyEventListener = this.onTelephonyEvent.bind(this);
+  }
+
+  renderedCallback() {
+    this.subscribeToVoiceToolkit();
+  }
+
+  changeHandler(event) {
+    this.payload = event.target.value;
+  }
+
+  onInvokeNBA(event) {
+    // Message must be in the format { key: 'value' }
+    // Use in the Next Best Action strategy filter element with $Request.key
+    // To learn more, see:
+    // https://help.salesforce.com/articleView?id=nba_strategy_expressions.htm&type=5
+    this.getToolkitApi().updateNextBestActions(
+      this.recordId,
+      JSON.parse(this.payload)
+    );
+  }
+
+  onSubscribe(event) {
+    this.subscribeToVoiceToolkit();
+  }
+
+  onUnsubscribe(event) {
+    this.unsubscribeFromVoiceToolkit();
+  }
+
+  subscribeToVoiceToolkit() {
+    const toolkitApi = this.getToolkitApi();
+    toolkitApi.addEventListener("callstarted", this.telephonyEventListener);
+    toolkitApi.addEventListener("callconnected", this.telephonyEventListener);
+    toolkitApi.addEventListener("callended", this.telephonyEventListener);
+    toolkitApi.addEventListener("mute", this.telephonyEventListener);
+    toolkitApi.addEventListener("unmute", this.telephonyEventListener);
+    toolkitApi.addEventListener("hold", this.telephonyEventListener);
+    toolkitApi.addEventListener("resume", this.telephonyEventListener);
+    toolkitApi.addEventListener(
+      "participantadded",
+      this.telephonyEventListener
+    );
+    toolkitApi.addEventListener(
+      "participantremoved",
+      this.telephonyEventListener
+    );
+    toolkitApi.addEventListener("swap", this.telephonyEventListener);
+    toolkitApi.addEventListener("conference", this.telephonyEventListener);
+    toolkitApi.addEventListener("pauserecording", this.telephonyEventListener);
+    toolkitApi.addEventListener("resumerecording", this.telephonyEventListener);
+    toolkitApi.addEventListener("transcript", this.telephonyEventListener);
+  }
+
+  unsubscribeFromVoiceToolkit() {
+    const toolkitApi = this.getToolkitApi();
+    toolkitApi.removeEventListener("callstarted", this.telephonyEventListener);
+    toolkitApi.removeEventListener(
+      "callconnected",
+      this.telephonyEventListener
+    );
+    toolkitApi.removeEventListener("callended", this.telephonyEventListener);
+    toolkitApi.removeEventListener("mute", this.telephonyEventListener);
+    toolkitApi.removeEventListener("unmute", this.telephonyEventListener);
+    toolkitApi.removeEventListener("hold", this.telephonyEventListener);
+    toolkitApi.removeEventListener("resume", this.telephonyEventListener);
+    toolkitApi.removeEventListener(
+      "participantadded",
+      this.telephonyEventListener
+    );
+    toolkitApi.removeEventListener(
+      "participantremoved",
+      this.telephonyEventListener
+    );
+    toolkitApi.removeEventListener("swap", this.telephonyEventListener);
+    toolkitApi.removeEventListener("conference", this.telephonyEventListener);
+    toolkitApi.removeEventListener(
+      "pauserecording",
+      this.telephonyEventListener
+    );
+    toolkitApi.removeEventListener(
+      "resumerecording",
+      this.telephonyEventListener
+    );
+    toolkitApi.removeEventListener("transcript", this.telephonyEventListener);
+  }
+
+  onTelephonyEvent(event) {
+    if (
+      (event.type === "callstarted" || event.type === "callconnected") &&
+      this.comboBoxRemoveParticipantValue === "Agent"
+    ) {
+      this.isTelephonyActionControlsDisabled = false;
+    }
+    if (event.type === "callended") {
+      this.isTelephonyActionControlsDisabled = true;
     }
 
-    changeHandler(event) {
-        this.payload = event.target.value;
+    if (event.type === "transcript") {
+      this.transcript = JSON.stringify(event.detail);
     }
 
-    onInvokeNBA(event) {
-        // Message must be in the format { key: 'value' }
-        // Use in the Next Best Action strategy filter element with $Request.key
-        // To learn more, see:
-        // https://help.salesforce.com/articleView?id=nba_strategy_expressions.htm&type=5
-        this.getToolkitApi().updateNextBestActions(this.recordId, JSON.parse(this.payload));
-    }
+    this.teleEvent = JSON.stringify(event);
+  }
 
-    onSubscribe(event) {
-        this.subscribeToVoiceToolkit();
-    }
+  getToolkitApi() {
+    return this.template.querySelector(
+      "lightning-service-cloud-voice-toolkit-api"
+    );
+  }
 
-    onUnsubscribe(event) {
-        this.unsubscribeFromVoiceToolkit();
-    }
+  get options() {
+    return [
+      { label: "INITIAL_CALLER", value: "Initial_Caller" },
+      { label: "THIRD_PARTY", value: "Third_Party" },
+    ];
+  }
 
-    subscribeToVoiceToolkit() {
-        const toolkitApi = this.getToolkitApi();
-        toolkitApi.addEventListener('callstarted', this.telephonyEventListener);
-        toolkitApi.addEventListener('callconnected', this.telephonyEventListener);
-        toolkitApi.addEventListener('callended', this.telephonyEventListener);
-        toolkitApi.addEventListener('mute', this.telephonyEventListener);
-        toolkitApi.addEventListener('unmute', this.telephonyEventListener);
-        toolkitApi.addEventListener('hold', this.telephonyEventListener);
-        toolkitApi.addEventListener('resume', this.telephonyEventListener);
-        toolkitApi.addEventListener('participantadded', this.telephonyEventListener);
-        toolkitApi.addEventListener('participantremoved', this.telephonyEventListener);
-        toolkitApi.addEventListener('swap', this.telephonyEventListener);
-        toolkitApi.addEventListener('conference', this.telephonyEventListener);
-        toolkitApi.addEventListener('pauserecording', this.telephonyEventListener);
-        toolkitApi.addEventListener('resumerecording', this.telephonyEventListener);
-        toolkitApi.addEventListener('transcript', this.conversationEventListener);
-    }
+  get endCallOptions() {
+    return [
+      { label: "AGENT", value: "Agent" },
+      { label: "INITIAL_CALLER", value: "Initial_Caller" },
+      { label: "THIRD_PARTY", value: "Third_Party" },
+    ];
+  }
 
-    unsubscribeFromVoiceToolkit() {
-        const toolkitApi = this.getToolkitApi();
-        toolkitApi.removeEventListener('callstarted', this.telephonyEventListener);
-        toolkitApi.removeEventListener('callconnected', this.telephonyEventListener);
-        toolkitApi.removeEventListener('callended', this.telephonyEventListener);
-        toolkitApi.removeEventListener('mute', this.telephonyEventListener);
-        toolkitApi.removeEventListener('unmute', this.telephonyEventListener);
-        toolkitApi.removeEventListener('hold', this.telephonyEventListener);
-        toolkitApi.removeEventListener('resume', this.telephonyEventListener);
-        toolkitApi.removeEventListener('participantadded', this.telephonyEventListener);
-        toolkitApi.removeEventListener('participantremoved', this.telephonyEventListener);
-        toolkitApi.removeEventListener('swap', this.telephonyEventListener);
-        toolkitApi.removeEventListener('conference', this.telephonyEventListener);
-        toolkitApi.removeEventListener('pauserecording', this.telephonyEventListener);
-        toolkitApi.removeEventListener('resumerecording', this.telephonyEventListener);
-        toolkitApi.removeEventListener('transcript', this.conversationEventListener);
-    }
+  get contactOptions() {
+    return [
+      { label: "PHONE NUMBER", value: "PhoneNumber" },
+      { label: "AGENT/QUEUE ID", value: "AgentOrQueueId" },
+    ];
+  }
 
-    onTelephonyEvent(event) {
-        if (event.type === 'callstarted' || event.type === 'callconnected') {
-            this.telephonyActionControlsDisabled = false;
-        }
-        if (event.type === 'callended') {
-            this.telephonyActionControlsDisabled = true;
-        }
-        this.teleEvent = event.type;
-    }
+  handleComboboxHoldChange(event) {
+    this.comboBoxHoldValue = event.detail.value;
+  }
 
-    onConversationEvent(event) {
-        // Handle a conversation-related event
-    }
+  handleComboboxResumeChange(event) {
+    this.comboBoxResumeValue = event.detail.value;
+  }
 
-    getToolkitApi() {
-        return this.template.querySelector('lightning-service-cloud-voice-toolkit-api');
-    }
+  handleComboboxRemoveParticipantChange(event) {
+    this.comboBoxRemoveParticipantValue = event.detail.value;
+  }
 
-    get options() {
-        return [
-            { label: 'INITIAL_CALLER', value: 'Initial_Caller' },
-            { label: 'THIRD_PARTY', value: 'Third_Party' }
-        ];
-    }
+  handleComboboxContactTypeChange(event) {
+    this.comboBoxContactTypeValue = event.detail.value;
+  }
 
-    get endCallOptions() {
-    	return [
-            { label: 'INITIAL_CALLER', value: 'Initial_Caller' },
-            { label: 'THIRD_PARTY', value: 'Third_Party' },
-    	    { label: 'AGENT', value: 'Agent' }
-        ];
-    }
+  handleComboboxContactTypeAddParticipantChange(event) {
+    this.comboBoxAddParticipantContactTypeValue = event.detail.value;
+  }
 
-    get contactOptions() {
-        return [
-            { label: 'PHONE NUMBER', value: 'PhoneNumber' },
-            { label: 'AGENT/QUEUE ID', value: 'AgentOrQueueId' }
-        ];
-    }
+  changePreviewCallHandler(event) {
+    this.previewPhoneNumber = event.target.value;
+  }
 
-    handleComboboxHoldChange(event) {
-        this.comboBoxHoldValue = event.detail.value;
-    }
+  changeAddParticipantHandler(event) {
+    this.addParticipantPhoneNumber = event.target.value;
+  }
 
-    handleComboboxResumeChange(event) {
-        this.comboBoxResumeValue = event.detail.value;
-    }
+  changeSendDigitsHandler(event) {
+    this.sendDigits = event.target.value;
+  }
 
-    handleComboboxRemoveParticipantChange(event) {
-        this.comboBoxRemoveParticipantValue = event.detail.value;
-    }
+  onMute() {
+    this.getToolkitApi().mute();
+  }
 
-    handleComboboxContactTypeChange(event) {
-        this.comboBoxContactTypeValue = event.detail.value;
-    }
+  onUnmute() {
+    this.getToolkitApi().unmute();
+  }
 
-    handleComboboxContactTypeAddParticipantChange(event) {
-        this.comboBoxAddParticipantContactTypeValue = event.detail.value;
-    }
+  onAcceptCall() {
+    this.getToolkitApi().acceptCall();
+  }
 
-    changePreviewCallHandler(event) {
-    	this.previewPhoneNumber = event.target.value;
-    }
+  onDeclineCall() {
+    this.getToolkitApi().declineCall();
+  }
 
-    changeAddParticipantHandler(event) {
-    	this.addParticipantPhoneNumber = event.target.value;
-    }
+  onEndCall() {
+    this.getToolkitApi().endCall(this.comboBoxRemoveParticipantValue);
+  }
 
-    handleBlindTransferChange(event) {
-    	this.isBlindTransfer = event.target.checked;        
-    }
+  onSendDigits() {
+    this.getToolkitApi().sendDigits(this.sendDigits);
+  }
 
-    changeSendDigitsHandler(event) {
-    	this.sendDigits = event.target.value;
-    }
- 
-    onMute() {
-        this.getToolkitApi().mute();
-    }
+  onPauseRecording() {
+    this.getToolkitApi().pauseRecording();
+  }
 
-    onUnmute() {
-        this.getToolkitApi().unmute();
-    }
+  onResumeRecording() {
+    this.getToolkitApi().resumeRecording();
+  }
 
-    onAcceptCall() {
-        this.getToolkitApi().acceptCall();
-    }
-    
-    onDeclineCall() {
-        this.getToolkitApi().declineCall();
-    }  
+  onHold() {
+    this.getToolkitApi().hold(this.comboBoxHoldValue);
+  }
 
-    onEndCall() {
-        this.getToolkitApi().endCall(this.comboBoxRemoveParticipantValue);
-    }
+  onResume() {
+    this.getToolkitApi().resume(this.comboBoxResumeValue);
+  }
 
-    onSendDigits() {
-    	this.getToolkitApi().sendDigits(this.sendDigits);
-    }
+  onSwap() {
+    this.getToolkitApi().swap();
+  }
 
-    onPauseRecording() {
-        this.getToolkitApi().pauseRecording();
-    }
+  onMerge() {
+    this.getToolkitApi().merge();
+  }
 
-    onResumeRecording() {
-        this.getToolkitApi().resumeRecording();
-    }
+  onStartPreviewCall() {
+    this.getToolkitApi().startPreviewCall(this.previewPhoneNumber);
+  }
 
-    onHold() {
-        this.getToolkitApi().hold(this.comboBoxHoldValue);
-    }
+  onAddParticipant() {
+    this.getToolkitApi().addParticipant(
+      this.comboBoxAddParticipantContactTypeValue,
+      this.addParticipantPhoneNumber,
+      false
+    );
+  }
 
-    onResume() {
-        this.getToolkitApi().resume(this.comboBoxResumeValue);
-    }
-
-    onSwap() {
-        this.getToolkitApi().swap();
-    }
-
-    onMerge() {
-        this.getToolkitApi().merge();
-    }
-
-    onStartPreviewCall() {
-        this.getToolkitApi().startPreviewCall(this.previewPhoneNumber);
-    }
-
-    onAddParticipant() {
-        this.getToolkitApi().addParticipant(this.comboBoxAddParticipantContactTypeValue, this.addParticipantPhoneNumber, this.isBlindTransfer);
-    }
+  onAddParticipantViaBlindTransfer() {
+    this.getToolkitApi().addParticipant(
+      this.comboBoxAddParticipantContactTypeValue,
+      this.addParticipantPhoneNumber,
+      true
+    );
+  }
 }
